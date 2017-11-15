dbd02cf4a3c8603ec648ed52d0746158dc5cef18
Lane Anderson <laneanderson92@gmail.com>
2017-11-15 00:43:08 -0600

    Finishes Reading Database and updating purchases.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,13 +2,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-//Create mysql connection
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: '3306',
-	user: 'root',
-	database: 'bamazon'
-});
+var connection;
 
 //Functions
 function productDisplay(jsonData){
@@ -17,10 +11,61 @@ function productDisplay(jsonData){
 	for (var i = 0; i < jsonData.length; i++){
 		console.log(jsonData[i].item_id, jsonData[i].product_name, jsonData[i].department_name, jsonData[i].price, jsonData[i].stock_quantity);
 	}
-
+	//Run productSelect function
+	productSelect(jsonData);
 }
 
+function productSelect(jsonData){
+	inquirer.prompt([
+
+		{
+			type: 'input',
+			name: 'chosen_item',
+			message: 'Which item would you like to purchase? (Type ID and hit Enter)'
+		},
+		{
+			type: 'input',
+			name: 'chosen_quantity',
+			message: 'How many would you like to purchase?'
+		}
+	]).then(function(answers){
+		console.log(answers)
+		productPurchase(answers.chosen_item, answers.chosen_quantity, jsonData);
+	});
+}
+
+function productPurchase(item, quantity, jsonData){
+	console.log(item, quantity);
+	if(checkQuantity(item, quantity, jsonData) === false){
+		console.log('\nSorry we don\'t seem to have that much in stock. Please search again.');
+	} else {
+		connection.query("UPDATE `bamazon`.`products` SET `stock_quantity`='"+checkQuantity(item, quantity, jsonData)+"' WHERE `item_id`='"+item+"';", function(err,res){
+			//run productDisplay function
+			console.log('\nYour Purchase has been made');
+		});
+	}
+	connection.end();
+	callDB();
+}
+
+function checkQuantity(item, reqQuantity, jsonData){
+	var currQuantity = jsonData[parseInt(item)-1].stock_quantity;
+	if (parseInt(currQuantity) >= parseInt(reqQuantity)){
+		return parseInt(currQuantity) - parseInt(reqQuantity);
+	} else {
+		return false;
+	};
+}
+
+
 function callDB(){
+	//Create mysql connection
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: '3306',
+		user: 'root',
+		database: 'bamazon'
+	});
 	//Connect to database
 	connection.connect(function(err){
 		if (err) {throw err;}
@@ -28,8 +73,8 @@ function callDB(){
 			//run productDisplay function
 			productDisplay(res)
 		});
-		connection.end();
 	});
 };
 
 callDB();
+
